0105b70918900666b76471cf5024adb31f12cf3c
STUDENT-PC\Student <umvr0736@gmail.com>
2021-12-16 15:55:25 +0800

    Spawn Point

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
+using Tanks;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
@@ -11,6 +12,7 @@ public class GameManager : MonoBehaviourPunCallbacks
     public static GameObject localPlayer;
     string gameVersion = "1";
 
+    private GameObject defaultSpawnPoint;
     void Awake()
     {
         if (instance != null)
@@ -24,6 +26,10 @@ public class GameManager : MonoBehaviourPunCallbacks
         PhotonNetwork.AutomaticallySyncScene = true;
         DontDestroyOnLoad(gameObject);
         instance = this;
+
+        defaultSpawnPoint = new GameObject("Default SpawnPoint");
+        defaultSpawnPoint.transform.position = new Vector3(0, 0, 0);
+        defaultSpawnPoint.transform.SetParent(transform, false);
     }
 
     void Start()
@@ -78,7 +84,39 @@ public class GameManager : MonoBehaviourPunCallbacks
             return;
         }
 
-        localPlayer = PhotonNetwork.Instantiate("TankPlayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
+
+        var spawnPoint = GetRandomSpawnPoint();
+        localPlayer = PhotonNetwork.Instantiate(
+            "TankPlayer", 
+            spawnPoint.position, 
+            spawnPoint.rotation,
+            0);
         Debug.Log("Player Intance ID" + localPlayer.GetInstanceID());
+    }
+
+    private Transform GetRandomSpawnPoint()
+    {
+        var spawnPoints = GetAllObjectsOfTypeInScene<SpawnPoint>();
+        return spawnPoints.Count == 0
+            ? defaultSpawnPoint.transform
+            : spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
+    }
+
+    public static List<GameObject> GetAllObjectsOfTypeInScene<T>()
+    {
+        var objectsInScene = new List<GameObject>();
+        foreach(var go in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if(go.hideFlags == HideFlags.NotEditable||
+                go.hideFlags == HideFlags.HideAndDontSave)
+            {
+                continue;
+            }
+            if(go.GetComponent<T>() != null)
+            {
+                objectsInScene.Add(go);
+            }
+        }
+        return objectsInScene;
     }
 }
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tanks
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
